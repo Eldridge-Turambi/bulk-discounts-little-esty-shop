e1977b31de6a31bb6521732d4198c324d14d86f9
Eldridge Turambi <eturambi95@gmail.com>
2022-01-16 14:23:56 -0700

    Created a master branch as a base repo

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -33,3 +33,4 @@ class GithubService
     pared = JSON.parse(response.body, symbolize_names: true)
   end
 end
+#This is the master branch and you need to delete the API stuff before you start this project
