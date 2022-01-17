d9400b22f3c9dedbafd31292d98f581cb2dc451c
Eldridge Turambi <eturambi95@gmail.com>
2022-01-17 01:51:21 -0700

    Create Bulk Discount Show spec

---
+++ spec/features/merchants/discounts/show_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant Bulk Discount Show Page' do
+
+  it 'visits bulk discount show page and sees the discount quantity threshold and percentage discount' do
+
+  end
+end 
